8319c85127b953514afe4ab5df76bacc2b3d3d7f
1nv8rZim <msf9542@rit.edu>
2024-09-21 17:41:13 -0400

    Add baseline functions

--- main.go
+++ main.go
@@ -14,3 +14,15 @@ type Field struct {
 	Default *string
 	Enum    *[]string
 }
+
+func Marshal(obj interface{}) ([]byte, error) {
+	panic("not implemented")
+}
+
+func Unmarshal(data []byte, obj interface{}) error {
+	panic("not implemented")
+}
+
+func Describe(obj interface{}) ([]*Field, error) {
+	panic("not implemented")
+}
